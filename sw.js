43eef8c3dce8bc4c83b2c40b2e86d5d1a39b50e5
sedonuam-ai <sedonuam@gmail.com>
2026-06-10 13:36:30 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const V = 'v1';
-const FILES = ['.','index.html','manifest.json','icons/icon-192.png','icons/icon-512.png'];
-self.addEventListener('install',  e => { e.waitUntil(caches.open(V).then(c=>c.addAll(FILES))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch',    e => { e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
